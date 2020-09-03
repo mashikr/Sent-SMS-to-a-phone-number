207d4a026b59070f899afcdf309e27fa7c7f28d0
Md Ashikur Rahman <43753561+mashikr@users.noreply.github.com>
2020-09-03 16:11:28 +0600

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('NUMBER', '+17603074184');
-define('S_ID', 'AC6d74fa4a39ab9f1062ee59bc86d1023a');
-define('TOKEN', 'b8f927e89ace1f4f79dc3fa822639ca8');
+define('NUMBER', 'Your number');
+define('S_ID', 'Your S_ID');
+define('TOKEN', 'Your Token');
